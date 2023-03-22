3f832e4f756a23132f3949c7a8f9073b7c781d2e
oleg <legik7127@gmail.com>
2023-03-22 10:10:58 +0600

    added method revertPrintList

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,11 +1,13 @@
 public class Program {
     public static void main(String[] args) {
-        myList l = new myList();
+        myDList l = new myDList();
         l.push(0, 1);
         l.push(1,2);
         l.push(0, 3);
         l.push(1, 4);
         l.push(4,5);
         l.printList();
+
+        l.revertPrintList();
     }
 }
